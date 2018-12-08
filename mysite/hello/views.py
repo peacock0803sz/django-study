5e24cb4d7054dae6ede8b1c9306be82a40223f59
Peacock0803sz <claude0803sz@gmail.com>
2018-12-09 01:02:01 +0900

    fix

--- mysite/hello/views.py
+++ mysite/hello/views.py
@@ -1,31 +1,51 @@
 from django.shortcuts import render
 from hello.forms import InputForm
-from .models import User
+from hello.models import User
 
 # Create your views here.
+
 def index(request):
     message = ''
+
     if request.method == 'POST':
         form = InputForm(request.POST)
+
+        if form.is_valid():
+            user_name = request.POST.get('user_name')
+            passwd = request.POST.get('passwd')
+
+            if User.objects.filter(user_name=user_name, passwd=passwd).exists():
+            # if user_name == 'admin' and passwd == 'admin':
+                message = 'ようそこ！' + user_name + 'さん'
+            else:
+                message = 'IDまたはパスワードが違います'
+    else:
+        form = InputForm()
+
+
+    return render(request, 'index.html', {'form': form, 'message': message})
+
+def register(request):
+    message = ''
+
+    if request.method == 'POST':
+        form = InputForm(request.POST)
+
         if form.is_valid():
             user_name = request.POST.get('user_name')
             passwd = request.POST.get('passwd')
 
             try:
-                user = User ()
+                user = User()
                 user.user_name = user_name
                 user.passwd = passwd
                 user.save()
-                message = user_name + 'welcome'
             except:
-                message = 'すでに同じユーザ名が存在します'
+                message = '同じユーザ存在します'
         else:
-                message = 'Error!'
+            message = 'エラー'
     else:
         form = InputForm()
-    return render (request, "index.html", {'form': form, 'message': message})
 
-if User.objects.filter(user_name=user_name, passwd=passwd).exists():
-    message = 'Welcome!' + user_name
-else:
-    message = 'Not found user name and password'
+
+    return render(request, 'register.html', {'form': form, 'message': message})
