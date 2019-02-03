90d084e5309acab752dbc703c26ba09049380cf9
peacock <peacock0803sz@gmail.com>
2019-02-03 20:00:37 +0900

    :pencil2: Fix a typo and mistake

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
-from django.shortcuts import render, get_object_or_404
 
 from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<link.pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
-
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
