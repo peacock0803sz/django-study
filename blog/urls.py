e8023832282fc360c76ed90623faa04ca1e074ef
peacock <peacock0803sz@gmail.com>
2019-02-03 17:40:05 +0900

    :art: Expend my app

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.shortcuts import render, get_object_or_404
+
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<link.pk/>', views.post_dateil, name='post_dateil')
 ]
+
+
+def post_dateil(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_dateil.html', {'post': post})
