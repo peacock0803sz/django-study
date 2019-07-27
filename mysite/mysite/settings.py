de30941c04974c9de1b3157de0f62d49ad5a6f28
peacock <peacock0803sz2gmail.com>
2019-07-28 03:00:38 +0900

    :wrench: Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -73,19 +73,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 db_from_env = dj_database_url.config()
-DATABASES = {
-    'default': dj_database_url.config()
-}
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-         "ENGINE": "django.db.backends.mysql",
-         "NAME": "kiitedb",
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
          "USER": "root",
          "PASSWORD": "root",
          "HOST": "127.0.0.1",
