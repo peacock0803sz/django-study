cc7cc499b8fc129361e35a73fc71e8f8a1a22de0
peacock <peacock0803sz2gmail.com>
2019-07-30 15:03:42 +0900

    :zap: Add DB settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
     }
 }
 
+# update db settings
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=400)
+DATABASES['default'].update(db_from_env)
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
